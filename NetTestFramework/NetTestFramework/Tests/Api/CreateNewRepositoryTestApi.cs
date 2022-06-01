318e7ecf7c0384624fd4f832e4f008fa434077e6
Dima Layeuski <dima.layeuski@gmail.com>
2022-06-01 14:57:26 +0300

    AQA-12: Create new repository test with correct name

---
+++ NetTestFramework/NetTestFramework/Tests/Api/CreateNewRepositoryTestApi.cs
@@ -0,0 +1,38 @@
+using System.Net;
+using FluentAssertions;
+using NetTestFramework.Clients;
+using NetTestFramework.Faker;
+using NetTestFramework.Models;
+using NetTestFramework.Services;
+using NLog;
+using NUnit.Framework;
+using Logger = NLog.Logger;
+
+namespace NetTestFramework.Tests.Api;
+
+public class CreateNewRepositoryTestApi : BaseTestApi
+{
+    private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+    private Project _project = null!;
+
+    [Test]
+    [Order(1)]
+    [TestCase(5)]
+    public void CreateNewRepository_NewRepositoryIsCreated(int lenghtOfRepositoryName)
+    {
+        _project = new ProjectFaker(lenghtOfRepositoryName).Generate();
+        var actualProject = ProjectService.AddRepository(_project);
+        _project = actualProject.Result;
+        _logger.Info(_project.ToString());
+        RestClientExtended.LastResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+    }
+
+    [OneTimeTearDown]
+    public void DeleteCreatedProject()
+    {
+        if (_project.Id != 0)
+        {
+            ProjectService.DeleteRepository(Configurator.Admin.Username, _project.Name);
+        }
+    }
+}
